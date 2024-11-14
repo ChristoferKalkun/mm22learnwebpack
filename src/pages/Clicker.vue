6f0c225215a55762400a4bb6b3b58413ec3478f2
Christofer Kalkun <50606160212@tptlive.ee>
2024-11-14 09:21:46 +0200

    2x boost

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -3,14 +3,20 @@
     <div class="col">
       <p class="text-center">Cookies {{ cookies }}</p>
       <img src="https://pngimg.com/d/cookie_PNG13656.png" class="img-fluid" @click="cookies++">
+    
+      <button @click="activateBoost" :disabled="boostActive || boostCooldown" class="btn btn-warning mt-3">
+        Double Cookie Boost
+      </button>
+      <p v-if="boostCooldown">Boost on cooldown...</p>
     </div>
+
     <div class="col">
-      Column
     </div>
+
     <div class="col">
       <button v-for="(upgrade, name) in upgrades"
         class="btn btn-outline-primary py-3"
-        :disabled="cookies<upgrade.price"
+        :disabled="cookies < upgrade.price"
         @click="buyUpgrade(upgrade)">
         Buy {{ name }} for 
         {{ upgrade.price }} clicks ({{upgrade.cps}} clicks per second)
@@ -22,32 +28,48 @@
 
 <script>
 export default {
-    created(){
-        setInterval(()=> {
-            for(const upgrade in this.upgrades){
-                this.cookies = (
-                    parseFloat(this.cookies) + this.upgrades[upgrade].cps * this.upgrades[upgrade].count
-                ).toFixed(1);
+    created() {
+        setInterval(() => {
+            for (const upgrade in this.upgrades) {
+                const cps = this.boostActive ? this.upgrades[upgrade].cps * 2 : this.upgrades[upgrade].cps;
+                this.cookies = (parseFloat(this.cookies) + cps * this.upgrades[upgrade].count).toFixed(1);
             }
         }, 1000);
     },
-    data(){
+    data() {
         return {
             cookies: 0,
+            boostActive: false,
+            boostCooldown: false,
             upgrades: {
-               cursor: {price: 10, cps: 0.1, count: 0},
-               grandma: {price: 100, cps: 1, count: 0},
-               farm: {price: 1000, cps: 10, count: 0},
-               factory: {price: 10000, cps: 100, count: 0},
-               temple: {price: 100000, cps: 1000, count: 0},
+                cursor: { price: 10, cps: 0.1, count: 0 },
+                grandma: { price: 100, cps: 1, count: 0 },
+                farm: { price: 1000, cps: 10, count: 0 },
+                factory: { price: 10000, cps: 100, count: 0 },
+                temple: { price: 100000, cps: 1000, count: 0 },
             }
         }
     },
     methods: {
-        buyUpgrade(upgrade){
-            if(this.cookies>=upgrade.price){
-                this.cookies-=upgrade.price;
-                upgrade.price += Math.ceil(upgrade.price*0.25)         
+        activateBoost() {
+            if (!this.boostCooldown) {
+                this.boostActive = true;
+                setTimeout(() => {
+                    this.boostActive = false;
+                    this.startCooldown();
+                }, 10000); 
+            }
+        },
+        startCooldown() {
+            this.boostCooldown = true;
+            setTimeout(() => {
+                this.boostCooldown = false;
+            }, 20000); 
+        },
+        buyUpgrade(upgrade) {
+            if (this.cookies >= upgrade.price) {
+                this.cookies -= upgrade.price;
+                upgrade.price += Math.ceil(upgrade.price * 0.25);
                 upgrade.count++;
             }
         }
